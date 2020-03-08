6441608e758fbe0e6405468c99851d95b4a6a906
sc <scbebetterme@gmail.com>
2020-03-08 22:33:47 +0800

    update

--- purge.sh
+++ purge.sh
@@ -8,8 +8,8 @@
  for txtfile in *.txt; do
  	filesize=`du -b "$txtfile" | cut -f1`
  	if [ "$filesize" -eq 0 ]; then
-	 	pdffile = ${txtfile%.txt}
-		jpgfile = $pdffile.jpg
+	 	pdffile=${txtfile%.txt}
+		jpgfile=$pdffile.jpg
  		echo "$txtfile" corrupted, removing "$txtfile", "$pdffile", "$jpgfile"
  		rm "$txtfile"
 		rm "../pdf/$pdffile"
